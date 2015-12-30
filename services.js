58eeb681480ea5f9badcae4aa5413018924a2163
juaniiie <becjuana@gmail.com>
2015-12-30 15:46:15 -0800

    Feature: Adds comments

--- services.js
+++ services.js
@@ -1,6 +1,14 @@
+/**
+ * Book Entry Factory.
+ * Returns books array, imageUrl function and isBookRead function
+ **/
 app.factory('Entry', 
 function() {
     
+    /**
+     * Chooses random image path
+     * Returns string
+     */
     var imageUrl = function() {
         var randNumber = function() {
             return Math.floor(Math.random() * 5) + 1;
@@ -29,6 +37,10 @@ function() {
         }
     ];
 
+    /**
+     * Checks if book object is in books array
+     * Returns boolean
+     */
     var isBookRead = function(bookEntry) {
         for (var i = 0; i < books.length; i++) {
             if (books[i].title.toLowerCase() === bookEntry.title.toLowerCase() &&
